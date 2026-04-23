c66424a604eb39e607763dfab541b8b6daf231b4
taiki-engineer <taiki.m0209@outlook.jp>
2026-04-23 19:09:31 +0900

    月移動追加

--- main.js
+++ main.js
@@ -111,6 +111,17 @@ function deleteTask(index) {
     renderTasks();
 };
 
+document.getElementById("prev").addEventListener("click", () => {
+    current.setMonth(current.getMonth() -1);
+    renderCalender();
+});
+
+
+document.getElementById("next").addEventListener("click", () => {
+    current.setMonth(current.getMonth() +1);
+    renderCalender();
+});
+
 renderCalender();
 
 renderCalender();
