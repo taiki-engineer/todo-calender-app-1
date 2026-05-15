f99f992015f1e22d9cf76af1c3cbd3187522e975
taiki-engineer <taiki.m0209@outlook.jp>
2026-05-15 23:39:32 +0900

    db作成途中

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const memos = [];
+// const memos = [];
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./memo.db");
 
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS memos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        text TEXT,
+        date TEXT
+        )
+    `);
+});
 
 app.get("/task", (req, res) => {
     res.json(memos);
@@ -15,10 +27,18 @@ app.get("/task", (req, res) => {
 
 
 app.post("/task", (req, res) => {
-    memos.push(req.body);
-    res.json({ message: "追加成功" });
+    const { text, date } = req.body;
+    db.run(
+        "INSERT INTO memos (text, date) VALUES (?, ?)",
+        [text, date],
+        (err) => {
+            if (err) {
+                return res.status(500).json({ error: err.massage});
+            }
+            res.json({ message: "追加成功" });
+        }
+    );
 });
-
 
 
 
